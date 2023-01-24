1a2b1e893b39d148e8637069d6c8c18daa847044
Eric Lin <lincheukyin_eric@yahoo.com.hk>
2023-01-24 01:48:44 -0600

    Added adaptive acoder and rcdm

--- src/algorithms/acoder.jl
+++ src/algorithms/acoder.jl
@@ -3,22 +3,24 @@ Implementation of A-CODER basic version.
 """
 
 
-struct ACoderParams
+struct ACODERParams
     L::Float64
     γ::Float64
 end
 
 
 function acoder_stepsize(a₋₁, A₋₁, L, γ)
-    _inside_sqrt = (1 + A₋₁ * γ)^2 + 8 * L * A₋₁ * (1 + A₋₁ * γ)
-    (1 + A₋₁ * γ + sqrt(_inside_sqrt)) / (4 * L)
+    # Largest value s.t. Line 4 of Algo is satisfied
+    _r = 0.4 * (1 + A₋₁ * γ) / L
+    _b, _c = - _r, - A₋₁ * _r
+    0.5 * (- _b + sqrt(_b^2 - 4 * _c))
 end
 
 
 function acoder(
     problem::CompositeFunc,
     exitcriterion::ExitCriterion,
-    parameters::ACoderParams;
+    parameters::ACODERParams;
     x₀=nothing
     )
 
@@ -61,15 +63,88 @@ function acoder(
         loss_func_grad_x₋₁ = problem.loss_func.grad(x₋₁)
         for j in m:-1:1
             # Step 7
-            if j+2 <= m
-                w[j+2] = y[j+2]
-            end
-            _grad_1_j = problem.loss_func.grad_block(w, j)
-            if j+1 <= m
+            if j <= m - 1
                 w[j+1] = y[j+1]
             end
-            _grad_2_j = problem.loss_func.grad_block(w, j)
-            p[j] = 0.5 * (_grad_1_j + _grad_2_j)
+            p[j] = problem.loss_func.grad_block(w, j)
+
+            # Step 8
+            q[j] = p[j] + a₋₁ / a * (loss_func_grad_x₋₁[j] - p₋₁[j])
+            
+            # Step 9
+            z[j] = z₋₁[j] + a * q[j]
+
+            # Step 10
+            v[j] = problem.reg_func.prox_opr_block(x₀[j] - z[j], A)
+
+            # Step 11
+            y[j] = A₋₁ / A * y₋₁[j] + a / A * v[j]
+        end
+        
+        iteration += m
+        if iteration % (m * exitcriterion.loggingfreq) == 0
+            elapsedtime = time() - starttime
+            optmeasure = problem.func_value(y)
+            @info "elapsedtime: $elapsedtime, iteration: $(iteration), optmeasure: $(optmeasure)"
+            logresult!(results, iteration, elapsedtime, optmeasure)
+            exitflag = checkexitcondition(exitcriterion, iteration, elapsedtime, optmeasure)
+        end
+    end
+
+    results
+end
+
+
+function acoder_adaptive(
+    problem::CompositeFunc,
+    exitcriterion::ExitCriterion,
+    parameters::ACODERParams;
+    x₀=nothing
+    )
+
+    # Init of ACODER
+    L, γ = parameters.L, parameters.γ
+    a, A = 0, 0
+    if isnothing(x₀)
+        x₀ = zeros(problem.d)
+    end
+    v, x, y = copy(x₀), copy(x₀), copy(x₀)
+    v₋₁, x₋₁, y₋₁ = copy(x₀), copy(x₀), copy(x₀)
+    q, w = zeros(length(x₀)), zeros(length(x₀))
+    p = problem.loss_func.grad(x₀); p₋₁ = copy(p)
+    z = zeros(problem.d); z₋₁ = copy(z)
+    m = problem.d  # Assume that each block is simply a coordinate for now
+    
+    # Run init
+    iteration = 0
+    exitflag = false
+    starttime = time()
+    results = Results()
+    init_optmeasure = problem.func_value(x₀)
+    logresult!(results, 1, 0.0, init_optmeasure)
+
+    while !exitflag
+
+        v₋₁ .= v; x₋₁ .= x; y₋₁ .= y
+        p₋₁ .= p; z₋₁ .= z
+        
+        # Step 4
+        A₋₁ = A
+        a₋₁ = a
+        a = acoder_stepsize(a, A₋₁, L, γ)
+        A = A₋₁ + a
+
+        # Step 5
+        x = A₋₁ / A * y + a / A * v
+
+        w .= x
+        loss_func_grad_x₋₁ = problem.loss_func.grad(x₋₁)
+        for j in m:-1:1
+            # Step 7
+            if j <= m - 1
+                w[j+1] = y[j+1]
+            end
+            p[j] = problem.loss_func.grad_block(w, j)
 
             # Step 8
             q[j] = p[j] + a₋₁ / a * (loss_func_grad_x₋₁[j] - p₋₁[j])
--- src/algorithms/rcdm.jl
+++ src/algorithms/rcdm.jl
@@ -11,6 +11,19 @@ struct RCDMParams
 end
 
 
+function get_L_probs(Ls, α)
+    _sum_L = 0.0
+    for _L in Ls
+        _sum_L += _L^α
+    end
+    _probs = zeros(length(Ls))
+    for (i, _L) in enumerate(Ls)
+        _probs[i] = _L^α / _sum_L
+    end
+    Categorical(_probs)
+end
+
+
 function rcdm(
     problem::CompositeFunc,
     exitcriterion::ExitCriterion,
@@ -27,6 +40,9 @@ function rcdm(
     x = copy(x₀)
     m = problem.d  # Assume that each block is simply a coordinate for now
 
+    # Pre-compute sampling probabilities
+    L_probs = get_L_probs(Ls, α)
+
     # Run init
     iteration = 0
     exitflag = false
@@ -36,7 +52,7 @@ function rcdm(
     logresult!(results, 1, 0.0, init_optmeasure)
 
     while !exitflag
-        j = rand(1:problem.d)  # TODO: Implement probability weight-based sampling
+        j = rand(L_probs)
         grad_j = problem.grad_block(x, j)
         x[j] = x[j] - grad_j / Ls[j]
         
@@ -50,5 +66,71 @@ function rcdm(
         end
     end
 
+    results
+end
+
+
+function rcdm_adapative(
+    problem::CompositeFunc,
+    exitcriterion::ExitCriterion,
+    parameters::RCDMParams;
+    x₀=nothing
+    )
+
+    # Init of RCDM
+    Ls = copy(parameters.Ls)
+    α = parameters.α
+    @assert(α == 0)
+    if isnothing(x₀)
+        x₀ = zeros(problem.d)
+    end
+    x = copy(x₀)
+    m = problem.d  # Assume that each block is simply a coordinate for now
+
+    # Pre-compute sampling probabilities
+    L_probs = get_L_probs(Ls, α)
+
+    # Run init
+    iteration = 0
+    lastloggediter = 0
+    exitflag = false
+    starttime = time()
+    results = Results()
+    init_optmeasure = problem.func_value(x₀)
+    logresult!(results, 1, 0.0, init_optmeasure)
+
+    while !exitflag
+        j = rand(L_probs)
+
+        # If smoothness parameter gets to zeros, skip block
+        if Ls[j] == 0.0
+            continue
+        end
+
+        grad_j = problem.grad_block(x, j)
+        _x_j = x[j]
+        x[j] = _x_j - grad_j / Ls[j]
+        _grad_j = problem.grad_block(x, j)
+
+        while grad_j * _grad_j < 0
+            Ls[j] = 2 * Ls[j]
+            x[j] = _x_j - grad_j / Ls[j]
+            _grad_j = problem.grad_block(x, j)
+            iteration += 1
+        end
+        Ls[j] = 0.5 * Ls[j]
+        
+        iteration += 2
+        if (iteration - lastloggediter) >= (m * exitcriterion.loggingfreq)
+            lastloggediter = iteration
+            elapsedtime = time() - starttime
+            optmeasure = problem.func_value(x)
+            @info "elapsedtime: $elapsedtime, iteration: $(iteration), optmeasure: $(optmeasure)"
+            logresult!(results, iteration, elapsedtime, optmeasure)
+            exitflag = checkexitcondition(exitcriterion, iteration, elapsedtime, optmeasure)
+        end
+    end
+
+    print(Ls)
     results
 end
