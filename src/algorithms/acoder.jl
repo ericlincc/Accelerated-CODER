d493dcc767c5498da34a3479dded014d4f1ed9c0
Eric Lin <lincheukyin_eric@yahoo.com.hk>
2023-01-24 13:39:30 -0600

    Added working acoder_eff

--- src/algorithms/acoder.jl
+++ src/algorithms/acoder.jl
@@ -95,6 +95,86 @@ function acoder(
 end
 
 
+function acoder_eff(
+    problem::CompositeFunc,
+    exitcriterion::ExitCriterion,
+    parameters::ACODERParams;
+    x₀=nothing
+    )
+
+    # Init of ACODER
+    L, γ = parameters.L, parameters.γ
+    a, A = 0, 0
+    if isnothing(x₀)
+        x₀ = zeros(problem.d)
+    end
+    v, x, y = copy(x₀), copy(x₀), copy(x₀)
+    v₋₁, x₋₁, y₋₁ = copy(x₀), copy(x₀), copy(x₀)
+    q, w = zeros(length(x₀)), zeros(length(x₀))
+    p = problem.loss_func.grad(x₀); p₋₁ = copy(p)
+    z = zeros(problem.d); z₋₁ = copy(z)
+    b_A_x = zeros(problem.loss_func.n)
+    m = problem.d  # Assume that each block is simply a coordinate for now
+    
+    # Run init
+    iteration = 0
+    exitflag = false
+    starttime = time()
+    results = Results()
+    init_optmeasure = problem.func_value(x₀)
+    logresult!(results, 1, 0.0, init_optmeasure)
+
+    while !exitflag
+
+        v₋₁ .= v; x₋₁ .= x; y₋₁ .= y
+        p₋₁ .= p; z₋₁ .= z
+        
+        # Step 4
+        A₋₁ = A
+        a₋₁ = a
+        a = acoder_stepsize(a, A₋₁, L, γ)
+        A = A₋₁ + a
+
+        # Step 5
+        x = A₋₁ / A * y + a / A * v
+
+        w .= x
+        loss_func_grad_x₋₁ = problem.loss_func.grad(x₋₁)
+        for j in m:-1:1
+            # Step 7
+            if j == m
+                p[j], b_A_x = problem.loss_func.grad_block_update!(x, j)
+            else
+                p[j], b_A_x = problem.loss_func.grad_block_update!(b_A_x, (j+1, y[j+1] - x[j+1]), j)
+            end
+
+            # Step 8
+            q[j] = p[j] + a₋₁ / a * (loss_func_grad_x₋₁[j] - p₋₁[j])
+            
+            # Step 9
+            z[j] = z₋₁[j] + a * q[j]
+
+            # Step 10
+            v[j] = problem.reg_func.prox_opr_block(x₀[j] - z[j], A)
+
+            # Step 11
+            y[j] = A₋₁ / A * y₋₁[j] + a / A * v[j]
+        end
+        
+        iteration += m
+        if iteration % (m * exitcriterion.loggingfreq) == 0
+            elapsedtime = time() - starttime
+            optmeasure = problem.func_value(y)
+            @info "elapsedtime: $elapsedtime, iteration: $(iteration), optmeasure: $(optmeasure)"
+            logresult!(results, iteration, elapsedtime, optmeasure)
+            exitflag = checkexitcondition(exitcriterion, iteration, elapsedtime, optmeasure)
+        end
+    end
+
+    results
+end
+
+
 function acoder_adaptive(
     problem::CompositeFunc,
     exitcriterion::ExitCriterion,
--- src/problems/loss_func/logistic_loss.jl
+++ src/problems/loss_func/logistic_loss.jl
@@ -5,6 +5,7 @@ struct LogisticLoss
     grad::Function
     grad_block::Function
     grad_block_sample::Function
+    grad_block_update!::Function
 
     function LogisticLoss(data::Data)
         n = length(data.values)
@@ -26,11 +27,32 @@ struct LogisticLoss
             return transpose(transpose(tmp) * A)
         end
         
-        function grad_block(x::Vector{Float64}, j)
+        function grad_block(x::Vector{Float64}, j::Int64)
             return grad(x)[j]
         end
 
-        function grad_block_sample(x::Vector{Float64}, j, t)
+        function grad_block_update!(
+            x::Vector{Float64},
+            j::Int64
+            )
+            A_x = A * x
+            b_A_x = b .* A_x
+            tmp = @. - b * exp(-b_A_x) / (exp(-b_A_x) + 1) / n
+            return transpose(transpose(tmp) * A)[j], b_A_x
+        end
+
+        function grad_block_update!(
+            b_A_x::Vector{Float64},
+            update_x::Tuple{Int64, Float64},
+            j::Int64,
+            )
+            i, Δxⁱ = update_x
+            b_A_x .+= b .* Δxⁱ .* A[:, i]
+            grad_block_x = sum(@. - b * exp(-b_A_x) / (1 + exp(-b_A_x)) * A[:, j]) / n
+            return grad_block_x, b_A_x
+        end
+
+        function grad_block_sample(x::Vector{Float64}, j::Int64, t::Int64)
             a_x = 0.0
             for i in 1:d
                 a_x += A[t, i] * x[i]
@@ -38,20 +60,7 @@ struct LogisticLoss
             b_a_x = b[t] * a_x
             return - b[t] * exp(-b_a_x) / (1 + exp(-b_a_x)) * A[t, j]
         end
-
-        # TODO: What is this implementation?
-        function coord_grad(grad_idx::Int64, update_idx::Int64 = -1, Δx::Float64 = 0.0)
-            A_x = A * x
-            b_A_x = b .* A_x
-            if update_idx !=  -1
-                b_A_x += Δx * (A[:, update_idx] .* b)
-            end
-            # vector
-            exp_A_x_b = @. exp(-b_A_x)
-            tmp = @. exp_A_x_b / (exp_A_x_b +1)
-            return transpose(A[:, grad_idx] .* b) * tmp / n
-        end
         
-        new(n, d, func_value, grad, grad_block, grad_block_sample)
+        new(n, d, func_value, grad, grad_block, grad_block_sample, grad_block_update!)
     end
 end
