bec06d6bba164efa850c26e4a5894d4c3afdf4d3
Eric Lin <lincheukyin_eric@yahoo.com.hk>
2023-01-24 17:35:03 -0600

    Replaced acoder and rcdm with acoder_eff and rcdm_eff

--- src/algorithms/rcdm.jl
+++ src/algorithms/rcdm.jl
@@ -45,52 +45,6 @@ function rcdm(
 
     # Run init
     iteration = 0
-    exitflag = false
-    starttime = time()
-    results = Results()
-    init_optmeasure = problem.func_value(x₀)
-    logresult!(results, 1, 0.0, init_optmeasure)
-
-    while !exitflag
-        j = rand(L_probs)
-        grad_j = problem.grad_block(x, j)
-        x[j] = x[j] - grad_j / Ls[j]
-        
-        iteration += 1
-        if iteration % (m * exitcriterion.loggingfreq) == 0
-            elapsedtime = time() - starttime
-            optmeasure = problem.func_value(x)
-            @info "elapsedtime: $elapsedtime, iteration: $(iteration), optmeasure: $(optmeasure)"
-            logresult!(results, iteration, elapsedtime, optmeasure)
-            exitflag = checkexitcondition(exitcriterion, iteration, elapsedtime, optmeasure)
-        end
-    end
-
-    results
-end
-
-
-function rcdm_eff(
-    problem::CompositeFunc,
-    exitcriterion::ExitCriterion,
-    parameters::RCDMParams;
-    x₀=nothing
-    )
-
-    # Init of RCDM
-    Ls = parameters.Ls
-    α = parameters.α
-    if isnothing(x₀)
-        x₀ = zeros(problem.d)
-    end
-    x = copy(x₀)
-    m = problem.d  # Assume that each block is simply a coordinate for now
-
-    # Pre-compute sampling probabilities
-    L_probs = get_L_probs(Ls, α)
-
-    # Run init
-    iteration = 0
     lastloggediter = 0
     exitflag = false
     starttime = time()
